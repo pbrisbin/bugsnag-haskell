90290afb34b0f69d2ce1b634a5e7eca59b7db9c8
patrick brisbin <pbrisbin@gmail.com>
2018-02-05 22:19:53 -0800

    Remove examples

--- examples/simple.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-module Main where
-
-import Network.Bugsnag
-
-main :: IO ()
-main = do
-    settings <- newBugsnagSettings "xxx"
-    notifyBugsnag settings $ bugsnagException "Error" "Some message" []
