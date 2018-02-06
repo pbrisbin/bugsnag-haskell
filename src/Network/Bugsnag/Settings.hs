341ff06bbb9fb5f47e4e2551483d388c532c0919
patrick brisbin <pbrisbin@gmail.com>
2018-02-05 21:32:15 -0800

    Document BugsnagSettings
    Closes #17.

--- src/Network/Bugsnag/Settings.hs
+++ src/Network/Bugsnag/Settings.hs
@@ -27,16 +27,52 @@ newtype BugsnagApiKey = BugsnagApiKey
     }
     deriving (FromJSON, IsString)
 
+-- | Notifier settings
+--
+-- See @'newBugsnagSettings'@.
+--
 data BugsnagSettings m = BugsnagSettings
     { bsApiKey :: BugsnagApiKey
+    -- ^ Your Integration API Key.
     , bsAppVersion :: Maybe Version
+    -- ^ The version of your application
+    --
+    -- Marking bugs as Fixed and having them auto-reopen in new versions
+    -- requires you set this.
+    --
     , bsReleaseStage :: BugsnagReleaseStage
+    -- ^ The current release-stage (Production, Development, etc)
     , bsNotifyReleaseStages :: [BugsnagReleaseStage]
+    -- ^ Which release-stages to notify in. By default Production and Staging.
     , bsBeforeNotify :: BugsnagEvent -> m BugsnagEvent
+    -- ^ Modify any events before they are sent
+    --
+    -- For example to attach a user, or set the context.
+    --
     , bsGroupingHash :: BugsnagEvent -> Maybe Text
+    -- ^ The grouping hash to use for any specific event
+    --
+    -- Events (exceptions) which have the same hash will be counted as the same
+    -- exception. Use @Nothing@ (the default) to maintain Bugsnags automatic
+    -- behavior (group by top in-project stack-frame).
+    --
     , bsIsInProject :: FilePath -> Bool
+    -- ^ Predicate for in-project stack frames
+    --
+    -- By default, all are considered in-project.
+    --
     , bsFilterStackFrames :: BugsnagStackFrame -> Bool
+    -- ^ Stack-fram filter
+    --
+    -- Return @True@ for any stack-frames that should be omitted from
+    -- notifications.
+    --
     , bsHttpManager :: Manager
+    -- ^ The HTTP @Manager@ used to emit notifications
+    --
+    -- It's more efficient, and ensures proper resource cleanup, to share a
+    -- single manager across an application. Must be TLS-enabled.
+    --
     }
 
 -- | Construct settings purely, given an existing @'Manager'@
