b4494765a3a095a7f82777e7524e6835ae2ba75c
patrick brisbin <pbrisbin@gmail.com>
2018-02-19 15:15:41 -0500

    Check X-Forwarded-For
    Closes #18.

--- src/Network/Bugsnag/Request.hs
+++ src/Network/Bugsnag/Request.hs
@@ -55,7 +55,31 @@ bugsnagRequestFromWaiRequest request = bugsnagRequest
     }
 
 requestRealIp :: Request -> Maybe ByteString
-requestRealIp request = lookup "X-Real-IP" $ requestHeaders request
+requestRealIp request = requestForwardedFor request
+    <|> lookup "X-Real-IP" (requestHeaders request)
+
+requestForwardedFor :: Request -> Maybe ByteString
+requestForwardedFor request = readForwardedFor
+    =<< lookup "X-Forwarded-For" (requestHeaders request)
+
+-- |
+--
+-- >>> readForwardedFor ""
+-- Nothing
+--
+-- >>> readForwardedFor "123.123.123"
+-- Just "123.123.123"
+--
+-- >>> readForwardedFor "123.123.123, 45.45.45"
+-- Just "123.123.123"
+--
+readForwardedFor :: ByteString -> Maybe ByteString
+readForwardedFor bs
+    | C8.null bs = Nothing
+    | otherwise = Just $ fst $ C8.break (== ',') bs
+    -- case C8.break (== ',') x of
+    --     ("", "") -> Nothing
+    --     (bs, _) -> Just bs
 
 requestUrl :: Request -> ByteString
 requestUrl request = requestProtocol
