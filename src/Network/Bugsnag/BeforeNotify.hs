ab84027cc1cfc9744533fdf60cdf5e6d6384cbc0
patrick brisbin <pbrisbin@gmail.com>
2018-03-09 19:07:37 -0500

    Add updateException

--- src/Network/Bugsnag/BeforeNotify.hs
+++ src/Network/Bugsnag/BeforeNotify.hs
@@ -1,6 +1,7 @@
 module Network.Bugsnag.BeforeNotify
     ( BeforeNotify
     , setStacktrace
+    , updateException
     , updateEventFromRequest
     , updateEventFromSession
     , updateEventFromWaiRequest
@@ -21,6 +22,28 @@ import Network.Bugsnag.StackFrame
 import Network.Wai (Request)
 
 type BeforeNotify = BugsnagEvent -> BugsnagEvent
+
+-- | Modify just the Exception part of an Event
+--
+-- Technically this will modify all exceptions in the Event, but if you're using
+-- this library normally, there will be only one.
+--
+-- This may be used to set more specific information for exception types in
+-- scope in your application:
+--
+-- > notifyBugsnagWith (updateException forSqlError) settings ex
+-- >
+-- > forSqlError :: BugsnagException -> BugsnagException
+-- > forSqlError ex = case fromException @SqlError ex of
+-- >    Just SqlError{..} -> ex
+-- >      { beErrorClass = "SqlError-" <> sqlErrorCode
+-- >      , beMessage = sqlErrorMessage
+-- >      }
+--
+updateException :: (BugsnagException -> BugsnagException) -> BeforeNotify
+updateException f event = event
+    { beExceptions = f <$> beExceptions event
+    }
 
 -- | Set the events @'BugsnagEvent'@ and @'BugsnagDevice'@
 updateEventFromWaiRequest :: Request -> BeforeNotify
@@ -57,9 +80,8 @@ setDevice device event = event
 -- > notifyBugsnagWith (setStacktrace [$(currentStackFrame) "myFunc"]) ...
 --
 setStacktrace :: [BugsnagStackFrame] -> BeforeNotify
-setStacktrace stacktrace event = event
-    { beExceptions = (\ex -> ex { beStacktrace = stacktrace })
-        <$> beExceptions event
+setStacktrace stacktrace = updateException $ \ex -> ex
+    { beStacktrace = stacktrace
     }
 
 -- | Set to @'ErrorSeverity'@
