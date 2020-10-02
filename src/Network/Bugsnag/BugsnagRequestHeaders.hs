97fdebe90f90e2c610073fe54f626bc8771d5ffc
patrick brisbin <pbrisbin@gmail.com>
2020-10-02 15:47:18 -0400

    Don't export unused map function

--- src/Network/Bugsnag/BugsnagRequestHeaders.hs
+++ src/Network/Bugsnag/BugsnagRequestHeaders.hs
@@ -1,12 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Network.Bugsnag.BugsnagRequestHeaders
-  ( BugsnagRequestHeaders
-  , bugsnagRequestHeaders
-  , bugsnagRequestHeadersToHeaders
-  , mapBugsnagRequestHeaders
-  , redactBugsnagRequestHeaders
-  )
- where
+    ( BugsnagRequestHeaders
+    , bugsnagRequestHeaders
+    , bugsnagRequestHeadersToHeaders
+    , redactBugsnagRequestHeaders
+    )
+where
 
 import Data.Aeson
 import Data.ByteString (ByteString)
@@ -21,13 +20,11 @@ newtype BugsnagRequestHeaders = BugsnagRequestHeaders {unBugsnagRequestHeaders :
     deriving (Show, Eq, Ord)
 
 instance ToJSON BugsnagRequestHeaders where
-    toJSON (BugsnagRequestHeaders headers) =
-        object $ map headerToText headers
-
-        where
-            -- Headers are most commonly ASCII, which UTF-8 is a superset of, so UTF-8 is fine to decode
-            headerToText :: (CI ByteString, ByteString) -> (Text, Value)
-            headerToText (name, value) = (TE.decodeUtf8 $ CI.original name, String $ TE.decodeUtf8 value)
+    toJSON (BugsnagRequestHeaders headers) = object $ map headerToText headers
+      where
+        headerToText :: (CI ByteString, ByteString) -> (Text, Value)
+        headerToText (name, value) =
+            (TE.decodeUtf8 $ CI.original name, String $ TE.decodeUtf8 value)
 
 -- | Create 'BugsnagRequestHeaders'
 bugsnagRequestHeaders :: RequestHeaders -> BugsnagRequestHeaders
@@ -37,16 +34,18 @@ bugsnagRequestHeaders = BugsnagRequestHeaders
 bugsnagRequestHeadersToHeaders :: BugsnagRequestHeaders -> RequestHeaders
 bugsnagRequestHeadersToHeaders = unBugsnagRequestHeaders
 
--- | Apply a function over each header in BugsnagRequestHeaders
-mapBugsnagRequestHeaders :: (Header -> Header) -> BugsnagRequestHeaders -> BugsnagRequestHeaders
-mapBugsnagRequestHeaders fn (BugsnagRequestHeaders headers) = BugsnagRequestHeaders $ map fn headers
-
 -- | For headers with the given names, replace their value with "<redacted>".
 --
 -- This is intended to remove sensitive data from headers.
-redactBugsnagRequestHeaders :: [HeaderName] -> BugsnagRequestHeaders -> BugsnagRequestHeaders
+redactBugsnagRequestHeaders
+    :: [HeaderName] -> BugsnagRequestHeaders -> BugsnagRequestHeaders
 redactBugsnagRequestHeaders redactList = mapBugsnagRequestHeaders redactHeader
   where
     redactHeader :: Header -> Header
     redactHeader (k, _) | k `elem` redactList = (k, "<redacted>")
     redactHeader h = h
+
+mapBugsnagRequestHeaders
+    :: (Header -> Header) -> BugsnagRequestHeaders -> BugsnagRequestHeaders
+mapBugsnagRequestHeaders fn (BugsnagRequestHeaders headers) =
+    BugsnagRequestHeaders $ map fn headers
