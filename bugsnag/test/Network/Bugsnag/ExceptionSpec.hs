3ada7fff5bed1a7427b3efc3667c6998938f234d
Restyled.io <commits@restyled.io>
2023-12-08 20:20:04 +0000

    Restyled by brittany

--- bugsnag/test/Network/Bugsnag/ExceptionSpec.hs
+++ bugsnag/test/Network/Bugsnag/ExceptionSpec.hs
@@ -88,7 +88,8 @@ spec = do
                 exception_stacktrace ex `shouldSatisfy` ((== 1) . length)
 
                 let frame = head $ exception_stacktrace ex
-                stackFrame_file frame `shouldBe` "test/Network/Bugsnag/ExceptionSpec.hs"
+                stackFrame_file frame
+                    `shouldBe` "test/Network/Bugsnag/ExceptionSpec.hs"
                 stackFrame_lineNumber frame `shouldBe` 83
                 stackFrame_columnNumber frame `shouldBe` Just 22
                 stackFrame_method frame `shouldBe` "checkpointCallStack"
